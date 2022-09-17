bf3cc9a124bf644378c2364e8d204be419467133
Mayara Silva <devmayara11@gmail.com>
2022-09-17 16:59:26 -0300

    Configurações: Informações

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Setting;
 
 class SettingController extends Controller
 {
@@ -14,12 +15,17 @@ class SettingController extends Controller
 
     public function index()
     {
-        return view('admin.settings.index');
+        $settings = [];
+
+        $dbsettings = Setting::get();
+
+        foreach($dbsettings as $dbsetting) {
+            $settings[$dbsetting['name']] = $dbsetting['content'];
+        }
+
+        return view('admin.settings.index', [
+            'settings' => $settings
+        ]);
     }
 
-    public function save()
-    {
-        return view('admin.settings.index');
-
-    }
 }
--- resources/views/Admin/settings/index.blade.php
+++ resources/views/Admin/settings/index.blade.php
@@ -15,35 +15,35 @@
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label" for="">Título do site</label>
                     <div class="col-sm-10">
-                        <input class="form-control @error('name') is-invalid @enderror" type="text" name="title" value="">
+                        <input class="form-control @error('name') is-invalid @enderror" type="text" name="title" value="{{$settings['title']}}">
                         {{-- <span class="error invalid-feedback">Por favor, insira seu nome completo.</span> --}}
                     </div>
                 </div>
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label" for="">Sub-título do site</label>
                     <div class="col-sm-10">
-                        <input class="form-control @error('name') is-invalid @enderror" type="text" name="subtitle" value="">
+                        <input class="form-control @error('name') is-invalid @enderror" type="text" name="subtitle" value="{{$settings['subtitle']}}">
                         {{-- <span class="error invalid-feedback">Por favor, insira seu nome completo.</span> --}}
                     </div>
                 </div>
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label" for="">E-mail para contato</label>
                     <div class="col-sm-10">
-                        <input class="form-control @error('name') is-invalid @enderror" type="email" name="email" value="">
+                        <input class="form-control @error('name') is-invalid @enderror" type="email" name="email" value="{{$settings['email']}}">
                         {{-- <span class="error invalid-feedback">Por favor, insira seu nome completo.</span> --}}
                     </div>
                 </div>
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label" for="">Cor do fundo</label>
                     <div class="col-sm-2">
-                        <input class="form-control @error('name') is-invalid @enderror" type="color" name="bgcolor" value="">
+                        <input class="form-control @error('name') is-invalid @enderror" type="color" name="bgcolor" value="{{$settings['bgcolor']}}">
                         {{-- <span class="error invalid-feedback">Por favor, insira seu nome completo.</span> --}}
                     </div>
                 </div>
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label" for="">Cor do texto</label>
                     <div class="col-sm-2">
-                        <input class="form-control @error('name') is-invalid @enderror" type="color" name="textcolor" value="">
+                        <input class="form-control @error('name') is-invalid @enderror" type="color" name="textcolor" value="{{$settings['textcolor']}}">
                         {{-- <span class="error invalid-feedback">Por favor, insira seu nome completo.</span> --}}
                     </div>
                 </div>
